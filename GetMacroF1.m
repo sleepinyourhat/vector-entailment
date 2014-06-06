376138aa13168a523dcd82008757e4196321aa91
Sam Bowman <sbowman@stanford.edu>
2014-06-06 09:39:22 -0700

    Temp

--- GetMacroF1.m
+++ GetMacroF1.m
@@ -5,14 +5,13 @@ R = [];
 
 for class = find([sum(confusion) ~= 0])
    R = [R, confusion(class, class) /  sum(confusion(:, class))];
+   if sum(confusion(class, :)) ~= 0
+    P = [P, confusion(class, class) /  sum(confusion(class, :))];
+   else
+    P = [P, 0];
+   end
 end
 
-for class = find([sum(confusion') ~= 0])
-   P = [P, confusion(class, class) /  sum(confusion(class, :))];
-end
-
-P
-R
 avP = mean(P);
 avR = mean(R);
 
