2d6ce4a67417715a8bf6517c9e1a4cdd1e65d656
Sam Bowman <sbowman@stanford.edu>
2014-10-08 17:36:29 -0700

    Update wordnet extractor.

--- synset-relations/wordnet_extractor.py
+++ synset-relations/wordnet_extractor.py
@@ -1,38 +1,46 @@
 from nltk.corpus import wordnet as wn
 from random import choice, uniform
 import sys
+import copy
+# Create a set of word--word relations based on wordnet nouns using a
+# tree traversal downwards from 'organism.n.01'
 
-# Create a set of word--word relations based on wordnet nouns.
+
+def addHyponymRelations(root, wn, relations, parents, vocab):
+    if not root.name.endswith('n.01') or '_' in root.name:
+        return
+
+    spl = root.name.split('.', 1)
+    rootString = spl[0]
+    vocab.add(rootString)
+
+    for parent in parents:
+        relations[(rootString, parent)] = "hyponym"
+        relations[(parent, rootString)] = "hypernym"
+
+    new_parents = copy.deepcopy(parents)
+    new_parents.append(rootString)
+
+    hyponyms = set()
+    for rel in root.hyponyms():
+        if rel.name.endswith('n.01') and not '_' in rel.name:
+            rel_string = rel.name.split('.', 1)[0]
+            addHyponymRelations(rel, wn, relations, new_parents, vocab)
+            hyponyms.add(rel_string)
+
+    for hyponym in hyponyms:
+        for cohyponym in hyponyms:
+            # Randomly downsample coordinate pairs from sets of coordinates of size > 8,
+            # with a sampling probability inversely proportional to squared
+            # set size.
+            if uniform(0, 1) < 256 / (len(hyponyms) ** 2) and cohyponym != hyponym:
+                relations[(hyponym, cohyponym)] = "coordinate"
 
 relations = {}
 vocab = set()
 
-for ss in wn.all_synsets():
-    if ss.name.endswith('n.01') and not '_' in ss.name:
-        spl = ss.name.split('.', 1)
-        main_string = spl[0]
-
-        for rel in ss.hypernyms() + ss.instance_hypernyms():
-            if rel.name.endswith('n.01') and not '_' in rel.name:
-                rel_string = rel.name.split('.', 1)[0]
-                relations[(main_string, rel_string)] = "hypernym"
-                vocab.add(main_string)
-
-        hyponyms = set()
-        for rel in ss.hyponyms() + ss.instance_hyponyms():
-            if rel.name.endswith('n.01') and not '_' in rel.name:
-                rel_string = rel.name.split('.', 1)[0]
-                relations[(main_string, rel_string)] = "hyponym"
-                hyponyms.add(rel_string)
-                vocab.add(main_string)
-
-        for hyponym in hyponyms:
-            for cohyponym in hyponyms:
-                # Randomly downsample coordinate pairs from sets of coordinates of size > 8,
-                # with a sampling probability inversely proportional to squared
-                # set size.
-                if uniform(0, 1) < 64 / (len(hyponyms) ** 2) and cohyponym != hyponym:
-                    relations[(hyponym, cohyponym)] = "coordinate"
+root = wn.synset('organism.n.01')
+addHyponymRelations(root, wn, relations, [], vocab)
 
 # Write the relations to STDOUT and the vocabulary to STDERR
 for key in relations.keys():
