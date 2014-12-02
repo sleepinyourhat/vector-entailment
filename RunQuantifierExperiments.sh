f3d8682ea86c4cb82eab14fbcfba1771ca231e75
Sam Bowman <sbowman@stanford.edu>
2014-12-02 13:10:00 -0800

    Paper update.

--- RunQuantifierExperiments.sh
+++ RunQuantifierExperiments.sh
@@ -35,8 +35,11 @@ export MATLABCMD="cd quant; lambda = 0.0001; dim = 25; td = 1; penult = 75; drop
 export MATLABCMD="cd quant; lambda = 0.0001; dim = 25; td = 1; penult = 75; dropout = 1; tot = 1; name='tq'; relu = 1; TrainModel(''\, 4\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
 export MATLABCMD="cd quant; lambda = 0.0001; dim = 25; td = 1; penult = 75; dropout = 1; tot = 1; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
 
-export MATLABCMD="cd quant; lambda = 0.0001; dim = 25; td = 1; penult = 75; dropout = 1; tot = 1; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
-export MATLABCMD="cd quant; lambda = 0.0001; dim = 25; td = 1; penult = 75; dropout = 1; tot = 0; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
+export MATLABCMD="cd quant; lambda = 0.0001; dim = 15; td = 1; penult = 75; dropout = 1; tot = 1; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
+export MATLABCMD="cd quant; lambda = 0.0001; dim = 15; td = 1; penult = 75; dropout = 1; tot = 0; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
+export MATLABCMD="cd quant; lambda = 0.0001; dim = 20; td = 1; penult = 75; dropout = 1; tot = 1; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
+export MATLABCMD="cd quant; lambda = 0.0001; dim = 20; td = 1; penult = 75; dropout = 1; tot = 0; name='tq'; relu = 1; TrainModel(''\, 5\, @Quantifiers\, name\, dim\, penult\, td\, lambda\, tot\, relu\, dropout\, 32);" ; qsub -v MATLABCMD quant/run.sh
+
 
 424 
 445
--- config/Sick.m
+++ config/Sick.m
@@ -223,14 +223,40 @@ elseif strcmp(dataflag, 'sick-plus-100k-ea')
                      './sick_data/SICK_trial_parsed_exactAlign.txt', ...
                      '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_100k.tsv'};
     hyperParams.testFilenames = {'./sick_data/SICK_test_annotated_rearranged_parsed_exactAlign.txt',...
-                     './sick_data/SICK_trial_parsed_18plusparens_exactAlign.txt', ...
+                     './sick_data/SICK_trial_parsed_exactAlign.txt', ...
                      '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_100.tsv'};
     hyperParams.splitFilenames = {};
     % Use different classifiers for the different data sources.
-    hyperParams.relationIndices = [1, 1, 4, 0, 0, 0; 1, 1, 1, 1, 1, 4; 0, 0, 0, 0, 0, 0];
+    hyperParams.relationIndices = [1, 1, 2; 1, 1, 2; 0, 0, 0];
+elseif strcmp(dataflag, 'sick-plus-600k-ea-c') 
+    % The number of relations.
+    hyperParams.numRelations = [3 2];
+
+    hyperParams.relations = {{'ENTAILMENT', 'CONTRADICTION', 'NEUTRAL'},
+                             {'ENTAILMENT', 'NONENTAILMENT'}};
+    relationMap = cell(2, 1);
+    relationMap{1} = containers.Map(hyperParams.relations{1}, 1:length(hyperParams.relations{1}));
+    relationMap{2} = containers.Map(hyperParams.relations{2}, 1:length(hyperParams.relations{2}));
+
+    wordMap = InitializeMaps('sick_data/all_sick_plus_t4.txt');
+    hyperParams.vocabName = 'aspt4';
+
+    hyperParams.trainingMultipliers = [(datamult * 6); (datamult * 6); 1];
+
+    hyperParams.trainFilenames = {'./sick_data/SICK_train_parsed_exactAlign.txt', ...
+                     './sick_data/SICK_train_parsed.txt', ...
+                     './sick_data/SICK_trial_parsed_exactAlign.txt', ...
+                     './sick_data/SICK_trial_parsed.txt', ...
+                     '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_600k.tsv'};
+    hyperParams.testFilenames = {'./sick_data/SICK_test_annotated_rearranged_parsed_exactAlign.txt',...
+                     './sick_data/SICK_trial_parsed_exactAlign.txt', ...
+                     '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_100.tsv'};
+    hyperParams.splitFilenames = {};
+    % Use different classifiers for the different data sources.
+    hyperParams.relationIndices = [1, 1, 1, 1, 2; 1, 1, 2, 0, 0; 0, 0, 0, 0, 0];
 elseif strcmp(dataflag, 'sick-plus-600k-ea') 
     % The number of relations.
-    hyperParams.numRelations = [3 7 2 2];
+    hyperParams.numRelations = [3 2];
 
     hyperParams.relations = {{'ENTAILMENT', 'CONTRADICTION', 'NEUTRAL'},
                              {'ENTAILMENT', 'NONENTAILMENT'}};
@@ -247,7 +273,7 @@ elseif strcmp(dataflag, 'sick-plus-600k-ea')
                      './sick_data/SICK_trial_parsed_exactAlign.txt', ...
                      '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_600k.tsv'};
     hyperParams.testFilenames = {'./sick_data/SICK_test_annotated_rearranged_parsed_exactAlign.txt',...
-                     './sick_data/SICK_trial_parsed_18plusparens_exactAlign.txt', ...
+                     './sick_data/SICK_trial_parsed_exactAlign.txt', ...
                      '/scr/nlp/data/ImageFlickrEntailments/shuffled_clean_parsed_entailment_pairs_100.tsv'};
     hyperParams.splitFilenames = {};
     % Use different classifiers for the different data sources.
