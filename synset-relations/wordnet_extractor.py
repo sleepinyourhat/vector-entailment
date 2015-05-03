ec8c81dd179d3a5b94c76914a6ceca357be518c2
Sam Bowman <sbowman@stanford.edu>
2015-05-03 11:18:27 -0700

    More reverts

--- synset-relations/wordnet_extractor.py
+++ synset-relations/wordnet_extractor.py
@@ -2,11 +2,11 @@ from nltk.corpus import wordnet as wn
 from random import choice, uniform
 import sys
 import copy
-# Create a set of word--word labels based on wordnet nouns using a
+# Create a set of word--word relations based on wordnet nouns using a
 # tree traversal downwards from 'organism.n.01'
 
 
-def addHyponymLabels(root, wn, labels, parents, vocab):
+def addHyponymRelations(root, wn, relations, parents, vocab):
     if not root.name.endswith('n.01') or '_' in root.name:
         return
 
@@ -15,8 +15,8 @@ def addHyponymLabels(root, wn, labels, parents, vocab):
     vocab.add(rootString)
 
     for parent in parents:
-        labels[(rootString, parent)] = "hyponym"
-        labels[(parent, rootString)] = "hypernym"
+        relations[(rootString, parent)] = "hyponym"
+        relations[(parent, rootString)] = "hypernym"
 
     new_parents = copy.deepcopy(parents)
     new_parents.append(rootString)
@@ -25,7 +25,7 @@ def addHyponymLabels(root, wn, labels, parents, vocab):
     for rel in root.hyponyms():
         if rel.name.endswith('n.01') and not '_' in rel.name:
             rel_string = rel.name.split('.', 1)[0]
-            addHyponymLabels(rel, wn, labels, new_parents, vocab)
+            addHyponymRelations(rel, wn, relations, new_parents, vocab)
             hyponyms.add(rel_string)
 
     for hyponym in hyponyms:
@@ -34,17 +34,17 @@ def addHyponymLabels(root, wn, labels, parents, vocab):
             # with a sampling probability inversely proportional to squared
             # set size.
             if uniform(0, 1) < 256 / (len(hyponyms) ** 2) and cohyponym != hyponym:
-                labels[(hyponym, cohyponym)] = "coordinate"
+                relations[(hyponym, cohyponym)] = "coordinate"
 
-labels = {}
+relations = {}
 vocab = set()
 
 root = wn.synset('organism.n.01')
-addHyponymLabels(root, wn, labels, [], vocab)
+addHyponymRelations(root, wn, relations, [], vocab)
 
-# Write the labels to STDOUT and the vocabulary to STDERR
-for key in labels.keys():
-    print labels[key] + "\t" + key[0] + "\t" + key[1]
+# Write the relations to STDOUT and the vocabulary to STDERR
+for key in relations.keys():
+    print relations[key] + "\t" + key[0] + "\t" + key[1]
 
 for word in vocab:
     sys.stderr.write(word + "\n")
